42528a9cd20a109649f72be6b9321e3f94bcd15b
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-07-20 03:07:25 +0400

    + implement store method in the controller

--- app/Http/Controllers/V1/Commands/CustomerCommandsController.php
+++ app/Http/Controllers/V1/Commands/CustomerCommandsController.php
@@ -2,24 +2,40 @@
 
 namespace App\Http\Controllers\V1\Commands;
 
+use App\Commands\CreateCustomerHandler;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateCustomerRequest;
+use App\Traits\Controllers\FormatsCustomerCommandResponses;
+use App\Traits\Controllers\FormatsErrorResponses;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
 
 class CustomerCommandsController extends Controller
 {
-    public function store()
+    use FormatsErrorResponses,
+        FormatsCustomerCommandResponses;
+
+    public function store(CreateCustomerRequest $request, CreateCustomerHandler $handler): Response
     {
-        // TODO
-        return response(['data' => 'store test']);
+        $result = $handler->handle($request->validated());
+
+        if (!$result) {
+            throw $this->errorResponse(
+                message: 'The requested operation failed',
+                code: SymfonyResponse::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+
+        return $this->createdSuccessfully();
     }
 
-    public function update()
+    public function update(): Response
     {
         // TODO
         return response(['data' => 'update test']);
     }
 
-    public function delete()
+    public function delete(): Response
     {
         // TODOs
         return response(['data' => 'delete test']);
