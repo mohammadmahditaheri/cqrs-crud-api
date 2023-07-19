268e935b6595fdfbe40e9a316a373fa6a6720cef
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-07-20 02:56:34 +0400

    + customer exists middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends HttpKernel
     /**
      * The application's global HTTP middleware stack.
      *
-     * These middleware are run during every request to your application.
+     * These middlewares are run during every request to your application.
      *
      * @var array<int, class-string|string>
      */
@@ -64,5 +64,10 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+
+        /**
+         * Custom middlewares
+         */
+        'customer_exists' => \App\Http\Middleware\CustomerExists::class
     ];
 }
---
+++ app/Http/Middleware/CustomerExists.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Contracts\Repositories\CustomerRepositoryInterface;
+use App\Traits\Controllers\FormatsErrorResponses;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CustomerExists
+{
+    use FormatsErrorResponses;
+    const ERROR_MESSAGE = 'The requested customer does not exist';
+
+    public function __construct(private CustomerRepositoryInterface $repository)
+    {}
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next): mixed
+    {
+        if (! $this->repository->find($request->route('customer'))) {
+            throw $this->errorResponse(
+                message: self::ERROR_MESSAGE
+            );
+        }
+
+        return $next($request);
+    }
+}
