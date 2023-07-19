bfed319d33c6b90022c49ab95af2265fe09508c7
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-07-19 19:53:52 +0400

    -+ configure phpunit and testing environment

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,36 @@
 
 namespace Tests;
 
+use Faker\Factory as Faker;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication,
+        DatabaseMigrations,
+        DatabaseTransactions;
+
+    protected \Faker\Generator $faker;
+
+    /**
+     * Set up the test
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->faker = Faker::create();
+    }
+
+    /**
+     * Reset the migrations
+     */
+    public function tearDown(): void
+    {
+        $this->artisan('migrate:reset');
+        parent::tearDown();
+    }
 }
