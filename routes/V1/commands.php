7f038d58bd4ac74e454353a38a263ee97546f0bc
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-07-20 06:06:05 +0400

    -+ conform routes to changes

--- routes/V1/commands.php
+++ routes/V1/commands.php
@@ -6,23 +6,23 @@
 |--------------------------------------------------------------------------
 */
 
-use App\Http\Controllers\V1\Commands\CustomerCommandsController;
+use App\Http\Controllers\V1\Commands\CustomerCommandController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::name('commands.')->group(function () {
     // create customer
-    Route::post('/customers', [CustomerCommandsController::class, 'store'])
+    Route::post('/customers', [CustomerCommandController::class, 'store'])
         ->name('customers.create');
 
     Route::middleware(['customer_exists'])->group(function () {
         // update customer
-        Route::put('/customers/{customer}', [CustomerCommandsController::class, 'update'])
+        Route::put('/customers/{customer}', [CustomerCommandController::class, 'update'])
             ->whereNumber('customer')
             ->name('customers.update');
 
         // delete customer
-        Route::delete('customers/{customer}', [CustomerCommandsController::class, 'destroy'])
+        Route::delete('customers/{customer}', [CustomerCommandController::class, 'destroy'])
             ->whereNumber('customer')
             ->name('customers.delete');
     });
