8382f4163cb27beeb794c9eac33e0bcc1429bdef
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-07-20 04:42:00 +0400

    -+ refactor repititive tests

--- tests/Feature/Commands/CreateCustomerCommandTest.php
+++ tests/Feature/Commands/CreateCustomerCommandTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Commands;
 
 use App\Traits\Controllers\FormatsCustomerCommandResponses;
+use Illuminate\Support\Str;
 use Illuminate\Testing\TestResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
@@ -57,113 +58,6 @@ class CreateCustomerCommandTest extends TestCase
         $this->assertDatabaseCount('customers', 1);
     }
 
-    /**
-     * @test
-     */
-    public function it_cannot_create_customer_without_first_name()
-    {
-        $this->data['first_name'] = null;
-
-        $response = $this->sendCreateRequest($this->data);
-
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
-            ->assertJson([
-                'message' => 'The first name field is required.',
-                'errors' => [
-                    'first_name' => ['The first name field is required.']
-                ]
-            ]);
-    }
-
-    /**
-     * @test
-     */
-    public function it_cannot_create_customer_without_last_name()
-    {
-        $this->data['last_name'] = null;
-
-        $response = $this->sendCreateRequest($this->data);
-
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
-            ->assertJson([
-                'message' => 'The last name field is required.',
-                'errors' => [
-                    'last_name' => ['The last name field is required.']
-                ]
-            ]);
-    }
-
-    /**
-     * @test
-     */
-    public function it_cannot_create_customer_without_date_of_birth()
-    {
-        $this->data['date_of_birth'] = null;
-
-        $response = $this->sendCreateRequest($this->data);
-
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
-            ->assertJson([
-                'message' => 'The date of birth field is required.',
-                'errors' => [
-                    'date_of_birth' => ['The date of birth field is required.']
-                ]
-            ]);
-    }
-
-    /**
-     * @test
-     */
-    public function it_cannot_create_customer_without_phone_number()
-    {
-        $this->data['phone_number'] = null;
-
-        $response = $this->sendCreateRequest($this->data);
-
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
-            ->assertJson([
-                'message' => 'The phone number field is required.',
-                'errors' => [
-                    'phone_number' => ['The phone number field is required.']
-                ]
-            ]);
-    }
-
-    /**
-     * @test
-     */
-    public function it_cannot_create_customer_without_email()
-    {
-        $this->data['email'] = null;
-
-        $response = $this->sendCreateRequest($this->data);
-
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
-            ->assertJson([
-                'message' => 'The email field is required.',
-                'errors' => [
-                    'email' => ['The email field is required.']
-                ]
-            ]);
-    }
-
-    /**
-     * @test
-     */
-    public function it_cannot_create_customer_without_bank_account_number()
-    {
-        $this->data['bank_account_number'] = null;
-
-        $response = $this->sendCreateRequest($this->data);
-
-        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
-            ->assertJson([
-                'message' => 'The bank account number field is required.',
-                'errors' => [
-                    'bank_account_number' => ['The bank account number field is required.']
-                ]
-            ]);
-    }
 
     /**
      * @test
@@ -212,7 +106,7 @@ class CreateCustomerCommandTest extends TestCase
         // first customer
         $this->sendCreateRequest($this->data);
 
-        // try for another customer with the same name and birth date
+        // try for another customer with the same name and birthdate
         $response = $this->sendCreateRequest($secondData);
 
         $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
@@ -222,5 +116,62 @@ class CreateCustomerCommandTest extends TestCase
                     'date_of_birth' => ['The date of birth combined with first name and last name has already been taken.']
                 ]
             ]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_cannot_create_customer_without_enough_data()
+    {
+        // first_name
+        $this->cantCreateWithoutMissing('first_name');
+
+        // last_name
+        $this->cantCreateWithoutMissing('last_name');
+
+        // date_of_birth
+        $this->cantCreateWithoutMissing('date_of_birth');
+
+        // phone_number
+        $this->cantCreateWithoutMissing('phone_number');
+
+        // email
+        $this->cantCreateWithoutMissing('email');
+
+        // bank_account_number
+        $this->cantCreateWithoutMissing('bank_account_number');
+    }
+
+    /**
+     * handle testing create with missing data
+     */
+    protected function cantCreateWithoutMissing(string $missing)
+    {
+        // missing
+        $response = $this->createWithMissing($missing);
+
+        $errorMessage = 'The ' . Str::replace('_', ' ', $missing) . ' field is required.';
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY)
+            ->assertJson([
+                'message' => $errorMessage,
+                'errors' => [
+                    $missing => [$errorMessage]
+                ]
+            ]);
+
+        // assert that the database isn't changed
+        $this->assertDatabaseEmpty('customers');
+    }
+
+    /**
+     * Send create request with incomplete data
+     */
+    private function createWithMissing(string $missing): TestResponse
+    {
+        $incompleteData = $this->data;
+        unset($incompleteData[$missing]);
+
+        return $this->sendCreateRequest($incompleteData);
     }
 }
