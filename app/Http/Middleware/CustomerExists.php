19d34dec8674fa6cf4d5aa01ee89de69d460dac0
Mohammad Mahdi Taheri <mmtaheri@yandex.com>
2023-07-20 04:03:42 +0400

    -+ clean up

--- app/Http/Middleware/CustomerExists.php
+++ app/Http/Middleware/CustomerExists.php
@@ -6,15 +6,16 @@ use App\Contracts\Repositories\CustomerRepositoryInterface;
 use App\Traits\Controllers\FormatsErrorResponses;
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class CustomerExists
 {
     use FormatsErrorResponses;
+
     const ERROR_MESSAGE = 'The requested customer does not exist';
 
     public function __construct(private CustomerRepositoryInterface $repository)
-    {}
+    {
+    }
 
     /**
      * Handle an incoming request.
@@ -25,7 +26,7 @@ class CustomerExists
      */
     public function handle(Request $request, Closure $next): mixed
     {
-        if (! $this->repository->find($request->route('customer'))) {
+        if (!$this->repository->find($request->route('customer'))) {
             throw $this->errorResponse(
                 message: self::ERROR_MESSAGE
             );
